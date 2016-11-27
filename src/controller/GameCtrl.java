5c64e1fd7c0722a73d3091caaced52fcc1306bc0
Robert <rbryan21@gmail.com>
2016-11-27 11:02:06 -0500

    Updated GameCtrl

--- src/controller/GameCtrl.java
+++ src/controller/GameCtrl.java
@@ -13,67 +13,55 @@ import model.PlayerDB;
 import model.RoomDB;
 import model.ValidInputDB;
 
-public class GameCtrl {
+public class GameCtrl 
+{
 
-	public GameCtrl() {
-		// TODO Auto-generated constructor stub
+	private PlayerCtrl player;
+	private PlayerDB pDB;
+	
+	public GameCtrl(int playerID) 
+	{
+		pDB = new PlayerDB();
+		this.player = pDB.getPlayer(playerID);
 	}
 	
-	public static void main(String[] args) throws SQLException {
-		
-	//	ValidInputDB db = new ValidInputDB();
-	//	ArrayList<ValidInputCtrl> aL = db.getValidInputsForMessage(50);
-	//	for (ValidInputCtrl v : aL)
-	//	{
-	//		System.out.println(aL.size());
-	//		System.out.println(v.toString());
-	//	}
-		
-	//	RoomDB roomDB = new RoomDB();
-	//	Map<Integer, Integer> map = roomDB.getStartingRoomsForAllLocations();
-	//	System.out.println(map.get(8));
-		
-			//PlayerCtrl p = new PlayerCtrl("Robsta");
-			
-			
-			//PlayerCtrl p2 = new PlayerCtrl("Jimmy");
-		//	PlayerDB pDB = new PlayerDB();
-			
-		
-	//	MonsterDB monsterDB = new MonsterDB();
-	//	MonsterCtrl m1 = monsterDB.getMonster(1);
-		//System.out.println(m1.getMonsterHitpoints());
-	//	int testDamage = monsterDB.damageMonster(m1.getMonsterID(), 13);
-		
-		
-	//	int[] intArray = monsterDB.getAllMonsterIDs();
-		
-				
-	//	PlayerDB playerDB = new PlayerDB();
-	//	playerDB.addUndefeatedMonsters(1);
-	//	playerDB.setMonsterDefeated(1, 1);
-		
-		
-	//	playerDB.addIncompleteLocations(1);
-	//	playerDB.setLocationCompleted(1, 1);
-	//	LocationDB lDB = new LocationDB();
-
-	//	int[] iArray = lDB.getAllLocationIDs();
-	//	System.out.println(iArray[8]);
-		
-	//	System.out.println(testDamage);
-		
-		//	PlayerCtrl p3 = pDB.getPlayer(1);
-		//	System.out.println(p3.getPlayerHitpoints()-10);
-		//	System.out.println(p3.toString());
-		//	System.out.println(p.getPlayerID() + " " + p.getInventoryID() + " " + p.getLocationID() + " " + p.getPlayerHitpoints() + " " + p.getPlayerName());
-		//	pDB.addPlayer(p2);
-		//	pDB.addPlayer(p2);
-		//	DBConnection dbc = new DBConnection();
-		//	dbc.modData(dbc, "INSERT INTO PLAYER(playerID, locationID, inventoryID, playerHitpoints, playerName) VALUES(1, 1, 1, 100, 'Timmy')");
-			
-	
+	public void startJungle()
+	{
 		
 	}
-
+	
+	public void startGlacier()
+	{
+		
+	}
+	
+	public void startSpaceship()
+	{
+		
+	}
+	
+	public void startVolcano()
+	{
+		
+	}
+	
+	public void startSpaceshipEscape()
+	{
+		
+	}
+	
+	public void startCave()
+	{
+		
+	}
+	
+	public void startDesert() 
+	{
+		
+	}
+	
+	public static void main(String[] args) throws SQLException 
+	{
+		
+	}
 }
